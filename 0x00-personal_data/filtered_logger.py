7935bcc76288e878bffe21be312a20585ddb49a0
Ignatius Kisekka <ignatiuskisekka@gmail.com>
2024-11-10 10:37:44 +0300

    User data

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -5,14 +5,18 @@
 import re
 from typing import List
 
+p = {
+    'cap': lambda x, y: r'(?P<field>{})=[^{}]*'.format(x, y),
+    'rep': lambda x: r'\g<field>={}'.format(x)
+}
+
 
 def filter_datum(
     fields: List[str], redaction: str, message: str, separator: str
 ) -> str:
     """Filters the data"""
     _fields = '|'.join([re.escape(field) for field in fields])
-    capture = r'(?P<field>{})=[^{}]*'.format(_fields, re.escape(separator))
-    replace = r'\g<field>={}'.format(re.escape(redaction))
+    capture, replace = p['cap'](_fields, separator), p['rep'](redaction)
     return re.sub(capture, replace, message)
 
 
