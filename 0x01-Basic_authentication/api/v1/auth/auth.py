110582949188352bc9fa74c575bb6a3503e005ca
Ignatius Kisekka <ignatiuskisekka@gmail.com>
2024-12-13 16:26:43 +0300

    3. Auth class

---
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""Module defines the Authentication implementation
+"""
+
+from flask import Request
+from typing import List, Optional, TypeVar
+
+T = TypeVar('T')
+
+
+class Auth:
+    """
+    Auth class that defines the authentication of the API
+
+    It defines the base implementation for other Authentication
+    types
+    """
+
+    def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+        """Checks if request requires authentication
+        """
+        return False
+
+    def authorization_header(self, request: Optional[Request] = None) -> Optional[str]:
+        """Gets the authentication header from the request
+        """
+        return None
+
+    def current_user(self, request: Optional[Request] = None) -> Optional[T]:
+        """Gets the logged in user
+        """
+        return None
