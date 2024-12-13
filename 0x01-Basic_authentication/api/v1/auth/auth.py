399554eeba25441ece02669a024a26ef83149faa
Ignatius Kisekka <ignatiuskisekka@gmail.com>
2024-12-13 16:58:15 +0300

    3. Auth class

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -15,8 +15,19 @@ class Auth:
 
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
         """Checks if request requires authentication
+
+        Args:
+            path: The path to Checks
+            excluded_paths: The paths which don't require authentication
+
+        Return:
+            (bool): True if path doesn't require auth else otherwise
         """
-        return False
+        if (path is None or excluded_paths is None or not isinstance(excluded_paths, List) or len(excluded_paths) == 0):
+            return True
+
+        path = path if path.endswith('/') else f'{path}/'
+        return (path in excluded_paths)
 
     def authorization_header(
         self, request=None
