37c869fb3412241566a41d81366fd90a4ba784f5
Ignatius K <ignatiuskisekka@gmail.com>
2025-01-22 11:42:58 +0300

    8. Basic - Base64 decode

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -3,6 +3,7 @@
 Module defines an implementation of Basic Authentication
 """
 
+import base64
 from typing import Optional
 from api.v1.auth.auth import Auth
 
@@ -38,3 +39,20 @@ class BasicAuth(Auth):
         if not authorization_header.startswith(self.PREFIX):
             return None
         return authorization_header[len(self.PREFIX):]
+
+    def decode_base64_authorization_header(
+        self, base64_authorization_header: str
+    ) -> Optional[str]:
+        """Decodes the content from the base64 header"""
+        if (
+                not base64_authorization_header or
+                not isinstance(base64_authorization_header, str)
+        ):
+            return None
+
+        encoded_header = base64_authorization_header.encode(encoding='utf-8')
+        try:
+            base64_decoded_header = base64.b64decode(encoded_header)
+            return base64_decoded_header.decode(encoding='utf-8')
+        except Exception:
+            return None
