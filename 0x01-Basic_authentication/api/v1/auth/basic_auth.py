13b1520bfde26a148c5eae32bfdadc6f4165aaa2
Ignatius K <ignatiuskisekka@gmail.com>
2025-01-22 11:13:33 +0300

    7. Basic - Base64 part

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -3,6 +3,7 @@
 Module defines an implementation of Basic Authentication
 """
 
+from typing import Optional
 from api.v1.auth.auth import Auth
 
 
@@ -11,5 +12,29 @@ class BasicAuth(Auth):
 
     This inherits from the base Auth implementation
     to add the basic authentication specification
+
+    Attributes:
+        PREFIX (str): The prefix of the authorization header
+
     """
-    ...
+    PREFIX = "Basic "
+
+    def extract_base64_authorization_header(
+        self, authorization_header: str
+    ) -> Optional[str]:
+        """Extract the content of the authorization header
+
+        Args:
+            authorization_header: The header to extract
+
+        Return:
+            The content of the header
+        """
+        if (
+                not authorization_header or
+                not isinstance(authorization_header, str)
+        ):
+            return None
+        if not authorization_header.startswith(self.PREFIX):
+            return None
+        return authorization_header[len(self.PREFIX):]
